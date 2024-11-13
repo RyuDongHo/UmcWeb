8384963858e5fd8127281f95bf63aad1442d2567
RyuDongHo <thffhahs03@gmail.com>
2024-11-14 00:11:16 +0900

    chore: style 수정 app 의 style파일을 정리하면서 appcontainer를 추가하여 sticky 속성이 똑바로 작동하도록 함

--- mission/chapter06/mission/src/0_App/index.jsx
+++ mission/chapter06/mission/src/0_App/index.jsx
@@ -1,17 +1,20 @@
 import Page from "../1_Page";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import GlobalStyles from "./globalStyle.js";
+import GlobalStyles from "./style/globalStyle.js";
 import Navbar from "./ui/Navbar/index.jsx";
+import STYLE from "./style/style.js";
 
-import theme from "./theme.js";
+import theme from "./style/theme.js";
 function App() {
   return (
     <ThemeProvider theme={theme.defaultTheme}>
       <GlobalStyles />
       <BrowserRouter>
-        <Navbar />
-        <Page />
+        <STYLE.AppContainer>
+          <Navbar />
+          <Page />
+        </STYLE.AppContainer>
       </BrowserRouter>
     </ThemeProvider>
   );
---
+++ mission/chapter06/mission/src/0_App/style/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const STYLE = {
+  AppContainer: styled.div`
+    position: relative;
+  `,
+};
+
+export default STYLE;
